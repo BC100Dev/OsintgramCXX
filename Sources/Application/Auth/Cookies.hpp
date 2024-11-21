fa8c2bbc249bdb2944c0b6a692121d76e7ec6886
BeChris100 <bechris.100@outlook.com>
2024-11-21 18:43:05 +0100

    Blueprint for 2FA

---
+++ Sources/Application/Auth/Cookies.hpp
@@ -0,0 +1,11 @@
+#ifndef OSINTGRAMCXX_COOKIES_HPP
+#define OSINTGRAMCXX_COOKIES_HPP
+
+namespace OsintgramCXX::Authentication {
+
+    class CookieInfo {
+    };
+
+}
+
+#endif //OSINTGRAMCXX_COOKIES_HPP
