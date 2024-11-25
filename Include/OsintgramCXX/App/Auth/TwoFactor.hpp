d17fddeeaa9c47d7f96168eb657b413f22834b8f
BeChris100 <bechris.100@outlook.com>
2024-11-25 12:30:33 +0100

    Little changes

--- Include/OsintgramCXX/App/Auth/TwoFactor.hpp
+++ Include/OsintgramCXX/App/Auth/TwoFactor.hpp
@@ -50,8 +50,8 @@ namespace OsintgramCXX::Authentication {
     // - code: the 2FA code
     // - method: The method that was used to enter the 2FA code (SMS / APP)
     // - initialResponseData: The first initial request that was made for authentication (/accounts/login/ajax)
-    // - sendCodeData: (presumably needed) request information that was used to send the code (if SMS)
-    // - cookies: The necessary authentication cookies
+    // - sendCodeData: (presumably needed) request information that was used to send the code (if SMS (Secure) is used)
+    // - userCookies: The necessary authentication cookies
     // - requestHeaders: The necessary request headers that holds the information for further requests
     TwoFA_Validation Validate2FA(const std::string &code, const TwoWayMethod &method,
                                  const ResponseData &initialResponseData, const ResponseData *sendCodeData,
--- Sources/Application/Auth/TwoFactor.cpp
+++ Sources/Application/Auth/TwoFactor.cpp
@@ -10,13 +10,6 @@ namespace OsintgramCXX::Authentication {
         return {};
     }
 
-    // this method passes onto several arguments:
-    // - code: the 2FA code
-    // - method: The method that was used to enter the 2FA code (SMS / APP)
-    // - initialResponseData: The first initial request that was made for authentication (/accounts/login/ajax)
-    // - sendCodeData: (presumably needed) request information that was used to send the code (if SMS)
-    // - userCookies: The necessary authentication cookies
-    // - requestHeaders: The necessary request headers that holds the information for further requests
     TwoFA_Validation Validate2FA(const std::string &code, const TwoWayMethod &method,
                                  const ResponseData &initialResponseData, const ResponseData *sendCodeData,
                                  std::vector<CookieInfo> &userCookies, const Headers &requestHeaders) {
